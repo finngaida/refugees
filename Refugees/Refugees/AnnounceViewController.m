d3fa413297a01cde1cfb19c5b6f2594336a7fb46
Finn Gaida <f@fga.pw>
2015-09-20 11:47:49 +0200

    add push + finalize register

--- Refugees/Refugees/AnnounceViewController.m
+++ Refugees/Refugees/AnnounceViewController.m
@@ -13,9 +13,16 @@
 
 @interface AnnounceViewController ()
 @property (nonatomic) CLLocationManager *manager;
+@property (nonatomic) JVFloatLabeledTextField *titleField;
+@property (nonatomic) JVFloatLabeledTextField *priceField;
+@property (nonatomic) JVFloatLabeledTextField *locationField;
+@property (nonatomic) JVFloatLabeledTextField *originField;
+@property (nonatomic) JVFloatLabeledTextView *descriptionField;
+@property (nonatomic) UIButton *submit;
 @end
 
 @implementation AnnounceViewController
+@synthesize titleField, priceField, locationField, descriptionField, originField, submit;
 
 const static CGFloat kJVFieldHeight = 44.0f;
 const static CGFloat kJVFieldHMargin = 10.0f;
@@ -41,7 +48,7 @@ const static CGFloat kJVFieldFloatingLabelFontSize = 11.0f;
     
     UIColor *floatingLabelColor = [UIColor brownColor];
     
-    JVFloatLabeledTextField *titleField = [[JVFloatLabeledTextField alloc] initWithFrame:CGRectZero];
+    titleField = [[JVFloatLabeledTextField alloc] initWithFrame:CGRectZero];
     titleField.font = [UIFont systemFontOfSize:kJVFieldFontSize];
     titleField.attributedPlaceholder =
     [[NSAttributedString alloc] initWithString:NSLocalizedString(@"Name", @"")
@@ -58,7 +65,7 @@ const static CGFloat kJVFieldFloatingLabelFontSize = 11.0f;
     [container addSubview:div1];
     div1.translatesAutoresizingMaskIntoConstraints = NO;
     
-    JVFloatLabeledTextField *priceField = [[JVFloatLabeledTextField alloc] initWithFrame:CGRectZero];
+    priceField = [[JVFloatLabeledTextField alloc] initWithFrame:CGRectZero];
     priceField.font = [UIFont systemFontOfSize:kJVFieldFontSize];
     priceField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"Time of Arrival", @"")
                                                                        attributes:@{NSForegroundColorAttributeName: [UIColor darkGrayColor]}];
@@ -72,7 +79,7 @@ const static CGFloat kJVFieldFloatingLabelFontSize = 11.0f;
     [container addSubview:div2];
     div2.translatesAutoresizingMaskIntoConstraints = NO;
     
-    JVFloatLabeledTextField *locationField = [[JVFloatLabeledTextField alloc] initWithFrame:CGRectZero];
+    locationField = [[JVFloatLabeledTextField alloc] initWithFrame:CGRectZero];
     locationField.font = [UIFont systemFontOfSize:kJVFieldFontSize];
     locationField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"Arrival Location", @"")
                                                                           attributes:@{NSForegroundColorAttributeName: [UIColor darkGrayColor]}];
@@ -81,12 +88,27 @@ const static CGFloat kJVFieldFloatingLabelFontSize = 11.0f;
     [container addSubview:locationField];
     locationField.translatesAutoresizingMaskIntoConstraints = NO;
     
+    
+    UIView *div4 = [UIView new];
+    div4.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.3f];
+    [container addSubview:div4];
+    div4.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    originField = [[JVFloatLabeledTextField alloc] initWithFrame:CGRectZero];
+    originField.font = [UIFont systemFontOfSize:kJVFieldFontSize];
+    originField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"Arrival Location", @"")
+                                                                          attributes:@{NSForegroundColorAttributeName: [UIColor darkGrayColor]}];
+    originField.floatingLabelFont = [UIFont boldSystemFontOfSize:kJVFieldFloatingLabelFontSize];
+    originField.floatingLabelTextColor = floatingLabelColor;
+    [container addSubview:originField];
+    originField.translatesAutoresizingMaskIntoConstraints = NO;
+    
     UIView *div3 = [UIView new];
     div3.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.3f];
     [container addSubview:div3];
     div3.translatesAutoresizingMaskIntoConstraints = NO;
     
-    JVFloatLabeledTextView *descriptionField = [[JVFloatLabeledTextView alloc] initWithFrame:CGRectZero];
+    descriptionField = [[JVFloatLabeledTextView alloc] initWithFrame:CGRectZero];
     descriptionField.font = [UIFont systemFontOfSize:kJVFieldFontSize];
     descriptionField.placeholder = NSLocalizedString(@"About yourself", @"");
     descriptionField.placeholderTextColor = [UIColor darkGrayColor];
@@ -95,9 +117,20 @@ const static CGFloat kJVFieldFloatingLabelFontSize = 11.0f;
     [container addSubview:descriptionField];
     descriptionField.translatesAutoresizingMaskIntoConstraints = NO;
     
+    submit = [[UIButton alloc] initWithFrame:CGRectMake(20, container.frame.size.height-220, container.frame.size.width-40, 100)];
+    submit.layer.masksToBounds = YES;
+    submit.layer.cornerRadius = 10;
+    submit.backgroundColor = [UIColor colorWithRed:(184.0/255.0) green:(233.0/255.0) blue:(134.0/255.0) alpha:1.0];
+    [submit addTarget:self action:@selector(submitData) forControlEvents:UIControlEventTouchUpInside];
+    [submit addTarget:self action:@selector(touchDown:) forControlEvents:UIControlEventTouchDown];
+    [submit addTarget:self action:@selector(releaseBtn:) forControlEvents:UIControlEventTouchUpInside];
+    submit.titleLabel.text = @"Submit";
+    [container addSubview:submit];
+    
     [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(xMargin)-[titleField]-(xMargin)-|" options:0 metrics:@{@"xMargin": @(kJVFieldHMargin)} views:NSDictionaryOfVariableBindings(titleField)]];
     [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[div1]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(div1)]];
     [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(xMargin)-[priceField]-(xMargin)-[div2(1)]-(xMargin)-[locationField]-(xMargin)-|" options:NSLayoutFormatAlignAllCenterY metrics:@{@"xMargin": @(kJVFieldHMargin)} views:NSDictionaryOfVariableBindings(priceField, div2, locationField)]];
+    [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(xMargin)-[originField]-(xMargin)-|" options:NSLayoutFormatAlignAllCenterY metrics:@{@"xMargin": @(kJVFieldHMargin)} views:NSDictionaryOfVariableBindings(div4, originField)]];
     [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[div3]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(div3)]];
     [container addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(xMargin)-[descriptionField]-(xMargin)-|" options:0 metrics:@{@"xMargin": @(kJVFieldHMargin)} views:NSDictionaryOfVariableBindings(descriptionField)]];
     
@@ -109,6 +142,25 @@ const static CGFloat kJVFieldFloatingLabelFontSize = 11.0f;
     
 }
 
+-(IBAction)touchDown:(id)sender {
+    UIButton *Btn = (UIButton*)sender;
+    
+    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.3 initialSpringVelocity:13 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        Btn.transform = CGAffineTransformMakeScale(0.8, 0.8);
+    }completion:nil];
+    
+    
+}
+
+-(IBAction)releaseBtn:(id)sender {
+    UIButton *Btn = (UIButton*)sender;
+    
+    [UIView animateWithDuration:0.3 delay:0.2 usingSpringWithDamping:0.3 initialSpringVelocity:13 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        Btn.transform = CGAffineTransformMakeScale(1, 1);
+        
+    }completion:nil];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -116,6 +168,19 @@ const static CGFloat kJVFieldFloatingLabelFontSize = 11.0f;
 
 - (IBAction)hide:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)submitData {
+    NSDictionary *data = @{@"name":titleField.text, @"origin":originField.text, @"arrivalTime":priceField.text, @"arrivalLoc":locationField.text, @"about":descriptionField.text};
+    
+    NSMutableURLRequest *req = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://192.168.87.152:8080/RefugeesWelcomeServer/refugees/createRefugee"]];
+    [req setHTTPMethod:@"POST"];
+    [req setHTTPBody:[NSKeyedArchiver archivedDataWithRootObject:data]];
+    
+    [[NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]] dataTaskWithRequest:req completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+    }];
+    
 }
 
 /*
--- Refugees/Refugees/AppDelegate.m
+++ Refugees/Refugees/AppDelegate.m
@@ -29,7 +29,27 @@
     // [Optional] Track statistics around application opens.
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     
+    // Register for Push Notitications
+    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                             categories:nil];
+    [application registerUserNotificationSettings:settings];
+    [application registerForRemoteNotifications];
+    
     return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
